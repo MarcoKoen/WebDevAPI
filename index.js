1e2f5d9f8378440dd514fb2d8d4785484de50b25
MarcoKoen <neokocram@gmail.com>
2021-11-04 15:37:27 +1300

    Added a Procfile

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const path = require('path');
+const fs = require('fs');
+
+const expressLayouts = require('express-ejs-layouts')
+
+
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(expressLayouts);
+
+app.get('/', (req, res) => {
+
+    res.send("Please refer to the documentation on how to use this API.")
+});
+
+app.get('/api/photos', (req, res) => {
+
+    let rawdata = fs.readFileSync('photos.json');
+    let photos = JSON.parse(rawdata);
+    console.log(photos);
+
+    res.send(photos)
+});
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
