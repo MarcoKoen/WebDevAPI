26dcdbc2e85fbb720523316ab0e2d24f4b062f99
MarcoKoen <neokocram@gmail.com>
2021-11-05 10:16:30 +1300

    Trying to get around CORS

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const app = express()
+
+res.header("Access-Control-Allow-Origin", "*");
+//app.use(function(req, res, next) {
+   // res.header("Access-Control-Allow-Origin", "*");
+   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //  next();
+//});
+
+
+
 const port = process.env.PORT || 3000
 const path = require('path');
 const fs = require('fs');
 
 const expressLayouts = require('express-ejs-layouts')
-
-res.header("Access-Control-Allow-Origin", "*");
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 
 app.set("view engine", "ejs");
 app.set('views', path.join(__dirname, 'views'));
@@ -33,9 +34,6 @@ app.get('/api/photos', (req, res) => {
 
     res.send(photos)
 });
-
-
-
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
