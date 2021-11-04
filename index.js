8222672e73ad51cb4a312b67a73139111cdac386
MarcoKoen <neokocram@gmail.com>
2021-11-04 16:23:57 +1300

    Trying to fix fetch error

--- index.js
+++ index.js
@@ -26,6 +26,9 @@ app.get('/api/photos', (req, res) => {
     res.send(photos)
 });
 
-
+app.use(function(req, res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
